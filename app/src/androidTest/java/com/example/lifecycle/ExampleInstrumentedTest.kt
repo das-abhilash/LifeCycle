379b58018910f1c1a84eeb317ab3f2bef0d692c5
abhilash das <abhilash.das@cred.club>
2023-09-03 11:35:06 +0530

    jbjbjb

--- app/src/androidTest/java/com/example/lifecycle/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/lifecycle/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package com.example.lifecycle
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,6 +16,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
+
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.lifecycle", appContext.packageName)
     }
